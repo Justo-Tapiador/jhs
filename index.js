3782adbec9f6add6cf5c26c07b7c59419a5fb45a
Justo Tapiador García <justo.tapiador@yahoo.com>
2022-12-24 10:09:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+var version = 'v' + require('./package.json').version;
+var versionGuard = function(version) {
+  if (version !== undefined) {
+    var message = 'More than one instance of JHS found. ' +
+      'Please make sure to require JHS and check that submodules do' +
+      ' not also include their own JHS dependency.';
+    throw new Error(message);
+  }
+};
+versionGuard(global._jhs);
+global._jhs = version;
+module.exports = require('./lib/execjhs');
