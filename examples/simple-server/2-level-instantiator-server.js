455f25c8aa157b9c0a55071578de243f401694d3
Justo Tapiador García <justo.tapiador@yahoo.com>
2023-01-19 09:47:44 +0100

    Add files via upload

--- examples/simple-server/2-level-instantiator-server.js
+++ examples/simple-server/2-level-instantiator-server.js
@@ -1,31 +1,44 @@
-/*****************************JUSTO-TAPIADOR-GARCIA*****************************
- ******* An Example of A Minimalist Secure Web Server (simple-server.js) *******
- *******      With JHS module, a Dynamic HTML pages Middleware Engine    *******
- *****************************JUSTO-TAPIADOR-GARCIA*****************************/
+/*
+const credits = {
+  author:'Justo Tapiador García',
+  mail:'justo.tapiador@yahoo.com',
+  scriptname:'2-level-server.js',           
+  desription: ' Example of Minimalist Secure Web Server' +
+              ' With JHS module, a Dynamic HTML pages Middleware Engine'+
+              ' configured with a 2-level hierarchical instanciator'
+}
+*/
 const os   = require('os'),
       path = require('path'),
       url  = require('url'),
-       JHS = require('jhs').instance;
+       JHS = require('jhs').instantiator;
      https = require("https"),
         fs = require("fs"),
  root_path = path.join(__dirname, 'wwwroot'),
  cert_path = path.join(__dirname, 'cert'),
       host = 'https://localhost',
       port = process.env.PORT || "4000",
-      home =	host+':'+port+'/';
+      home =	host+':'+port+'/',
+      filename =__filename.split('\\').slice(-1)[0].split('.')[0];
   
 var  flags    = {home: '.', path:'/',querystring:''},
-     execJHS = new JHS(),
+     execJHS = new JHS({levels:3}),
      options = {home:home,root:"./wwwroot",method:'GET'},
      server   = https.createServer({key: fs.readFileSync(cert_path+"\\key.pem"),cert: fs.readFileSync(cert_path+"\\cert.pem")});
+
+     execJHS.banModules(1,['bitcore-lib']);
+
+    
 
 server.listen(port, ()=>{
       console.log(
 `\t\x1b[97m${os.hostname()}\x1b[0m (\x1b[90mOperating System\x1b[0m)
-\tis running \x1b[91mThis webServer
+\tis running \x1b[91mThis '${filename}
 \t\x1b[0m\x1b[92mwhich Listens to Requests
 \ton \x1b[0m\x1b[93m${home}\x1b[0m`
           );
+  console.log('JHS Instantiator',execJHS.getInfo());
+  //console.log('credits',credits);
 });
 
 server.on('request', function(request, response) {
@@ -40,8 +53,9 @@ server.on('request', function(request, response) {
               p = path.parse(p);
               options.scriptname = file;
           if(p.ext == '.jhs'){
-              execJHS.parseFile(file,false, options, function(jhsResult, err){  
-                response.end(jhsResult);
+              execJHS.parse(file,false, options, function(jhsResult, err){  
+                    response.end(jhsResult);
+                  
               });     
           }else {
             try{
@@ -49,9 +63,10 @@ server.on('request', function(request, response) {
             }catch(e){
               file = options.root+'/index.jhs';
               options.scriptname = file;
-              execJHS.parseFile(file,false, options, function(jhsResult, err){  
-                response.end(jhsResult);
-              }); 
+              execJHS.parse(file,false, options, function(jhsResult, err){  
+                    response.end(jhsResult);
+               
+              });   
           }
         }
           break;
--- examples/simple-server/2-level-server.js
+++ examples/simple-server/2-level-server.js
@@ -1,6 +1,7 @@
 /*****************************JUSTO-TAPIADOR-GARCIA*****************************
- ******* An Example of A Minimalist Secure Web Server (simple-server.js) *******
+ ******* An Example of A Minimalist Secure Web Server (2-level-server.js) *******
  *******      With JHS module, a Dynamic HTML pages Middleware Engine    *******
+ *******            instanciated in 2-level hierarchical mode            *******
  *****************************JUSTO-TAPIADOR-GARCIA*****************************/
 const os   = require('os'),
       path = require('path'),
@@ -12,20 +13,26 @@ const os   = require('os'),
  cert_path = path.join(__dirname, 'cert'),
       host = 'https://localhost',
       port = process.env.PORT || "4000",
-      home =	host+':'+port+'/';
+      home =	host+':'+port+'/',
+      filename =__filename.split('\\').slice(-1)[0].split('.')[0];
   
 var  flags    = {home: '.', path:'/',querystring:''},
-     execJHS = new JHS(),
+     execJHS_1 = new JHS({delimiters:['<?#1jhs','?#1>']}),
+     execJHS_0 = new JHS(),
+
      options = {home:home,root:"./wwwroot",method:'GET'},
      server   = https.createServer({key: fs.readFileSync(cert_path+"\\key.pem"),cert: fs.readFileSync(cert_path+"\\cert.pem")});
+
+    
 
 server.listen(port, ()=>{
       console.log(
 `\t\x1b[97m${os.hostname()}\x1b[0m (\x1b[90mOperating System\x1b[0m)
-\tis running \x1b[91mThis webServer
+\tis running \x1b[91mThis '${filename}
 \t\x1b[0m\x1b[92mwhich Listens to Requests
 \ton \x1b[0m\x1b[93m${home}\x1b[0m`
           );
+          console.log('JHS set into instance mode');
 });
 
 server.on('request', function(request, response) {
@@ -40,8 +47,10 @@ server.on('request', function(request, response) {
               p = path.parse(p);
               options.scriptname = file;
           if(p.ext == '.jhs'){
-              execJHS.parseFile(file,false, options, function(jhsResult, err){  
-                response.end(jhsResult);
+              execJHS_1.parseFile(file,false, options, function(jhsResult_1, err){  
+                execJHS_0.parseFile(jhsResult_1,true, options, function(jhsResult_0, err){  
+                    response.end(jhsResult_0);
+                  });  
               });     
           }else {
             try{
@@ -49,9 +58,11 @@ server.on('request', function(request, response) {
             }catch(e){
               file = options.root+'/index.jhs';
               options.scriptname = file;
-              execJHS.parseFile(file,false, options, function(jhsResult, err){  
-                response.end(jhsResult);
-              }); 
+              execJHS_1.parseFile(file,false, options, function(jhsResult_1, err){  
+                execJHS_0.parseFile(jhsResult_1,true, options, function(jhsResult_0, err){  
+                    response.end(jhsResult_0);
+                  });  
+              });   
           }
         }
           break;
